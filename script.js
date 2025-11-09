415ac289c9f21a0ab6c1160d4c0123721fdd171f
kapil rana <rana.kapil9@gmail.com>
2025-11-09 17:48:37 +0530

    Fixed PDF URL to use raw GitHub link for direct PDF viewing

--- script.js
+++ script.js
@@ -71,8 +71,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 rating: 5,
                 description: 'A delightful collection of stories told by a grandmother to her grandchildren during summer vacation',
                 assignedTo: ['student', 'teacher'],
-                pdf: 'https://kapilrana1.github.io/inter-college-bazpur-library/books/Grandma%27s%20Bag%20of%20Stories%20by%20Sudha%20Murthy.pdf',
-                pdfRaw: 'https://raw.githubusercontent.com/kapilrana1/inter-college-bazpur-library/main/books/Grandma%27s%20Bag%20of%20Stories%20by%20Sudha%20Murthy.pdf'
+                pdf: 'https://raw.githubusercontent.com/kapilrana1/inter-college-bazpur-library/main/books/Grandma%27s%20Bag%20of%20Stories%20by%20Sudha%20Murthy.pdf'
             }
         ];
         // Save default books to localStorage
